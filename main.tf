6e982b44542bac89e77e4bfdbe6ee2a91f121013
srinipolaganitech <cloudaifocus@gmail.com>
2025-11-26 23:23:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "gcs-bucket-1" {
-  name          = "tf-demo-bkt-001"
+  name          = "tf-demo-bkt-002"
   location      = "US"
   force_destroy = true
   project = "argon-gear-478416-a0"
